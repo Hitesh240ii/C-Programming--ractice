e8f4da0e3148e2aaa0ecd57eabe3bc4099e0d185
Hitesh240ii <hiteshshimpi2006@gmail.com>
2024-06-11 22:47:27 +0530

    factorial

---
+++ while loop/factorial.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main(void)
+{
+    int i=1;
+    int num;
+    int factorial =1;
+
+    printf("enter a number :");
+    scanf("%d",&num);
+
+    if (num<0)
+    {
+        printf("enter valid integer value");
+    }
+    else
+    {
+        while(i<=num)
+        {
+            factorial*=i;
+            i++;
+        }
+    }
+    printf("value of factorial is %d",factorial);
+    return 0;
+}
