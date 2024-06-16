774a68c4b616c3db00260ba40f680a566a81e8a9
Hitesh240ii <hiteshshimpi2006@gmail.com>
2024-06-16 22:44:55 +0530

    for loop

---
+++ 5.for loop/syntax.c
@@ -0,0 +1,21 @@
+/*
+
+for (expression 1; expression 2; expression 3)
+{
+
+    code to be executed
+
+
+}
+
+
+//Expression 1 is executed (one time) before the execution of the code block.
+
+//Expression 2 defines the condition for executing the code block.
+
+//Expression 3 is executed (every time) after the code block has been executed.
+
+
+
+
+*/
