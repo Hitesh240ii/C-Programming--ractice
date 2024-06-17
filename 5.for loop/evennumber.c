1b8e18f3c0f0941401d5d637a5989c557d810340
Hitesh240ii <hiteshshimpi2006@gmail.com>
2024-06-17 11:24:37 +0530

    even number

---
+++ 5.for loop/evennumber.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i ;
+
+
+    printf("even number from 1 to 50 are");
+    
+    for ( i=0 ; i<=50 ; i+=2)
+    {
+        printf("%d\n",i);
+    }
+
+
+
+    return 0;
+}
