b5ec7210e77f68438c341a13ed2aeeeaadf995a4
Hitesh240ii <hiteshshimpi2006@gmail.com>
2024-06-19 21:32:46 +0530

    comments added

--- 5.for loop/evennumber.c
+++ 5.for loop/evennumber.c
@@ -7,7 +7,7 @@ int main(void)
 
     printf("even number from 1 to 50 are");
     
-    for ( i=0 ; i<=50 ; i+=2)
+    for ( i=0 ; i<=50 ; i+=2)// initialize with 0 then give condition upto which we have to print even number then increment i by 2 since even number...
     {
         printf("%d\n",i);
     }
