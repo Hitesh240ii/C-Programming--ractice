7e7aba4fd91ceaec004d0d2926a7ae154efdcca7
Hitesh240ii <hiteshshimpi2006@gmail.com>
2024-06-19 21:37:04 +0530

    comments added

--- 5.for loop/factorial.c
+++ 5.for loop/factorial.c
@@ -1,8 +1,10 @@
+//for factorial 3*2*1
 #include <stdio.h>
 
 int main(void)
 {
-
+    //declare 3 variables i ,factorial , num
+    // i for increment factorial for initialization as 1 and num for the input from user
     int i;
     int factorial=1;
     int num;
@@ -10,7 +12,7 @@ int main(void)
 
     printf("Enter the number:");
     scanf("%d",&num);
-
+//check input is nonzero since factorial is of only positive integers
     if(num<0)
     {
         printf("Factorial cant be of negative integers");
@@ -19,7 +21,7 @@ int main(void)
     {
         for (i=1; i<=num ; i++)
         {
-        factorial*=i;
+        factorial*=i; //value of factorial is 1 and i is increasing after each loop then multiply the with factorial and add it in factorial
         }
         printf("Factorial of %d is %d",num,factorial);
     }
