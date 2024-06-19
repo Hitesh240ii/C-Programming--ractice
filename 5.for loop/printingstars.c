c0cfc4c3b96971ac171dda99a780f85f8cf0581b
Hitesh240ii <hiteshshimpi2006@gmail.com>
2024-06-19 21:39:44 +0530

    comments added

--- 5.for loop/printingstars.c
+++ 5.for loop/printingstars.c
@@ -9,9 +9,9 @@ int main(void)
     printf("enter the number of rows:");
     scanf("%d",&num);
 
-    for(i=1; i<=num; i++)
+    for(i=1; i<=num; i++)//outer loop for the number of rows -- number of rows = input from user
     {
-        for (j = 1 ; j<=i ; j++)
+        for (j = 1 ; j<=i ; j++)// inner loop for printing stars 
         {
             printf("*");
         }
