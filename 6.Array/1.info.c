029d87a1ef1c90a5e1841713dced5d3fe79398f0
Hitesh240ii <hiteshshimpi2006@gmail.com>
2024-06-21 10:59:30 +0530

    array

---
+++ 6.Array/1.info.c
@@ -0,0 +1,25 @@
+//What is array?
+
+        // array is collection of data items of same type eg. pack of 10 pens 
+
+        //array are used to store multiple values of same data type in single variavble
+
+// Create an array
+
+        //To create an array, define the data type (like int) and specify the name of the array followed by square brackets [].
+
+        //To insert values to it, use a comma-separated list, inside curly braces
+
+        int MyNum[] ={ 5, 10, 15 ,20}; //this is array
+
+//Access elements of an array
+        //To access an array element, refer to its index number.
+        
+        //Array indexes start with 0: [0] is the first element. [1] is the second element, etc.
+
+        //printf("%d",MyNum[0]); this will print 5
+
+        //To change an array elements refer to its index number //eg. MyNum[0]=55
+
+// items are stored one after another 
+
