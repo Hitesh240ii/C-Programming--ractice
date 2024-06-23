0d63c6ed5693ae1c34e359795d2480863721d2e5
Hitesh240ii <hiteshshimpi2006@gmail.com>
2024-06-23 18:53:06 +0530

    sum

---
+++ 7.Multidimensional Array/summatrices.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+ {
+    // Define two 2x2 matrices
+    int matrix1[2][2] = {{1, 2}, {3, 4}};
+    int matrix2[2][2] = {{5, 6}, {7, 8}};
+    int result[2][2];  // Resultant matrix
+    
+    // Add matrices element-wise
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            result[i][j] = matrix1[i][j] + matrix2[i][j];
+        }
+    }
+    
+    // Display the result matrix
+    printf("Resultant Matrix:\n");
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            printf("%d ", result[i][j]);
+        }
+        printf("\n");
+   
+}
