86712b89858afd9c26eb232a0f34537ddf46bba8
Hitesh240ii <hiteshshimpi2006@gmail.com>
2024-06-24 07:29:43 +0530

    special string character

---
+++ 8.Strings/SpecialCharacter.c
@@ -0,0 +1,16 @@
+/*
+
+    \' - for adding single quote in string
+    \" - For adding double quote in string
+    \\ - For adding BackSlash
+eg:
+     char txt[] = "We are the so-called \"Vikings\" from the north.";
+     printf("%s", txt);
+
+    \n - for adding a new line
+    \t - for adding indentation 
+
+%c is used to input only one character
+%s is used to input a string
+
+*/
