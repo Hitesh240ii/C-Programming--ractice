92c88034ce2f6a902653771ce0c94f6e0fd1cbb0
Hitesh240ii <hiteshshimpi2006@gmail.com>
2024-06-25 13:57:09 +0530

    null pointer

--- 9.Pointers/intro.c
+++ 9.Pointers/intro.c
@@ -4,6 +4,8 @@
 
 // The address of the variable you are working with is assigned to the pointer:
 
+//stored value is hexadecimal 
+
 #include <stdio.h>
 
 int main()
@@ -12,8 +14,17 @@ int main()
     int var = 42;
     int *ptr = &var; //*ptr will is the declaration of the pointer and ptr holds the address 
 
+    int *ptr2 = NULL ; // this is NULL pointer
     printf("%d\n",var);  // prints the var i.e 42
-    printf("%p",ptr);  // prints the address of var 
+
+     
+    printf("This is the address of the variable var %p\n", ptr);  // prints the address of var or
+    printf("This is another way of printing address of var %p\n", &var);
+
+    printf("This is NUll pointer %p\n",ptr2);
+
+    printf("The address of the pointer is %p", &ptr);
+    // use %d and *ptr to get the corresponding address value eg. this will print 42 after changing
 
     return 0;
 }
