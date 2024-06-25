d4b34bb9b6c0da4ff5a1e4212a86f99ef34e3f98
Hitesh240ii <hiteshshimpi2006@gmail.com>
2024-06-25 14:42:16 +0530

    POINTER ARRAY

---
+++ 9.Pointers/pointerarray.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(void)
+{
+    int arr[] = { 1,2,37,4,5,6,7};
+    printf("Value at positoin 3 of the array is %d\n",arr[2]);
+
+    printf("The address of the value at position 1 is %d\n",&arr[0]);
+    printf("The address of the value at position 2 is %d\n",&arr[1]);
+    //or
+    printf("The address of the value at position 1 is %d\n",arr);
+    printf("The address of the value at position 2 is %d\n", arr+1);
+
+
+    printf("The  VALUE address of the value at position 1 is %d\n",*(&arr[0]));
+    printf("The  VALUE address of the value at position 2 is %d\n",*(&arr[1]));
+    //or
+    printf("The VALUE  address of the value at position 1 is %d\n",*(arr));
+    printf("The VALUE  address of the value at position 2 is %d\n", *(arr+1));
+    
+    return 0;
+}
