fa692ea1938c274c6e29df4316fc581f41a7aa41
Hitesh240ii <hiteshshimpi2006@gmail.com>
2024-06-26 20:02:16 +0530

    sum of numbers

---
+++ 10.Functions/SumOfNum.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int num1;
+int num2;
+void Sum()
+{
+    printf("ENter first number:");
+    scanf("%d",&num1);
+
+    printf("Enter second number:");
+    scanf("%d",&num2);
+
+    printf("The sum of the given numbers is : %d",num1 + num2);
+}
+
+
+int main(void)
+{
+    Sum();
+    return 0;
+}
