7ec1beba6d35f571a850b6db7d23d368563f151e
Hitesh240ii <hiteshshimpi2006@gmail.com>
2024-06-28 18:47:13 +0530

    maxm new

--- 10.Functions/maxm.c
+++ 10.Functions/maxm.c
@@ -1,15 +1,39 @@
+
 #include <stdio.h>
+int num1,num2,num3;
+void maxm(int num1,int num2,int num3)
+{
 
-int max(int a, int b);
-
-int main() {
-    int num1 = 10;
-    int num2 = 15;
-    int result = max(num1, num2);
-    printf("The maximum of %d and %d is %d\n", num1, num2, result);
-    return 0;
+    if (num1>num2 && num1 >num3)
+    {
+        printf("%d is greater",num1);
+    }
+    else if( num2> num3 && num2> num1)
+    {
+        printf("%d is greater",num2);
+    }
+    else if (num1== num2 && num2 ==num3)
+    {
+        printf("all numbers are equal");
+    }
+    else if ( num1== num2 || num2 == num3 || num1 == num3)
+    {
+        printf("two numbers are equal");
+    }
+    else
+    {
+        printf("%d is greater",num3);
+    }
 }
 
-int max(int a, int b) {
-    return (a > b) ? a : b;
+int main(void)
+{
+    printf("enter number 1:");
+    scanf("%d",&num1);
+    printf("enter number 2:");
+    scanf("%d",&num2);
+    printf("enter number 3:");
+    scanf("%d",&num3);
+    maxm(num1,num2,num3);
+    return 0;
 }
