e9affd078637aaa9600e83d004c231f32623e489
Hitesh240ii <hiteshshimpi2006@gmail.com>
2024-06-28 18:58:04 +0530

    lowest

---
+++ 10.Functions/lowest.c
@@ -0,0 +1,39 @@
+
+#include <stdio.h>
+int num1,num2,num3;
+void lowest(int num1,int num2,int num3)
+{
+
+    if (num1<num2 && num1 <num3)
+    {
+        printf("%d is  lowest",num1);
+    }    
+    else if( num2< num3 && num2< num1)
+    {
+        printf("%d is lowest",num2);
+    }
+    else if (num1== num2 && num2 ==num3)
+    {
+        printf("all numbers are equal");
+    }
+    else if ( num1== num2 || num2 == num3 || num1 == num3)
+    {
+        printf("two numbers are equal");
+    }
+    else
+    {
+        printf("%d is  lowest",num3);
+    }
+}
+
+int main(void)
+{
+    printf("enter number 1:");
+    scanf("%d",&num1);
+    printf("enter number 2:");
+    scanf("%d",&num2);
+    printf("enter number 3:");
+    scanf("%d",&num3);
+    lowest(num1,num2,num3);
+    return 0;
+}
