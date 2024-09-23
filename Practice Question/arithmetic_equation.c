600372d18e27933f307e15cd85120f9d50c638be
Hitesh240ii <hiteshshimpi2006@gmail.com>
2024-09-23 20:12:54 +0530

    AP

---
+++ Practice Question/arithmetic_equation.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int num1,term ; // DIFFERENCE IS 2 , FIRST TERM IS 1 , AND THE FORUMULA IS 
+                    // Nth term = first term +( n - 1 )*d
+                    
+    printf("Enter the number of the term you want :");
+    scanf("%d", &num1);
+
+    term = 1 + (num1 - 1) * 2 ;
+
+    printf("The %d-th term of the arithmetic sequence is %d",num1 ,term);
+    
+    return 0;
+}
