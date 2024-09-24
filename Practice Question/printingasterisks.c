51f92184cd95fbb9522dcafd9b63851fa3d427ca
Hitesh240ii <hiteshshimpi2006@gmail.com>
2024-09-24 23:27:52 +0530

    asterisks

---
+++ Practice Question/printingasterisks.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int numOfAsterisks;
+    int i = 0;
+
+    printf("Enter a number of asterisks you would like to see: ");
+    scanf("%d", &numOfAsterisks);
+
+    while (i < numOfAsterisks)
+    {
+        printf("*");
+        i++;
+    }
+    printf("\n");
+
+    return 0;
+}
