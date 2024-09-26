96bd8bf20f80ad46095483d97a361be85bcc71e6
Hitesh240ii <hiteshshimpi2006@gmail.com>
2024-09-26 20:10:26 +0530

    basic for loop

---
+++ 5.for loop/punishment.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void)
+//printing sorry hundred times
+{
+    for (int i = 1 ; i <= 100 ; i++)
+    {
+        printf("%d. Sorry for the mistake!!!\n", i);
+    }
+
+    return 0;
+}
